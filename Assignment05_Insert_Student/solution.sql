ccf97723bb61fcefd651a877999725e25a2d2817
1U25CA079-VSakthiganesh <thalapathysakthi51@gmail.com>
2026-07-15 09:49:25 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,9 +1,6 @@
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+INSERT INTO Student(studentID,StudentName,Gender,DepartmentID)
+VALUES
+(1001,'Arun','Male',101),
+(1002,'Divya','Female',102),
+(1003,'Karthick','Male',101);
+SELECT*FROM Student;
